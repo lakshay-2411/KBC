c0ae764d25b39a2cee57193333c03d4e4cc0399b
lakshay-2411 <lakshaynandwani06@gmial.com>
2024-04-23 16:00:07 +0530

    Changed questions in the json files

--- script.js
+++ script.js
@@ -148,8 +148,8 @@ function submitAnswer(correctAnswer) {
             quizContainer.style.opacity = '1';
             submitContainer.style.opacity = '1';
             displayNextQuestion();
-        }, 2000);
-    }, 3000);
+        }, 1000);
+    }, 1000);
 }
 
 function checkAnswer(selectedOption, correctAnswer) {
